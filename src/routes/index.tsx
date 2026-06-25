0413ac667d78185b474e943dc5c001ed066778f0
Juan David Merchan Torres <juan1014166@gmail.com>
2026-06-25 16:55:36 -0500

    feat: add home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,11 +4,51 @@ export const Route = createFileRoute('/')({ component: Home });
 
 function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
+    <main className="p-10">
+      <h1 className="my-4 text-4xl font-bold">Home</h1>
+
+      <h2 className="my-4 text-3xl font-bold">Introduction</h2>
+      <p>
+        Lorem Ipsum es simplemente el texto de relleno de las imprentas y
+        archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de
+        las industrias desde el año 1500, cuando un impresor (N. del T. persona
+        que se dedica a la imprenta) desconocido usó una galería de textos y los
+        mezcló de tal manera que logró hacer un libro de textos especimen. No
+        sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno
+        en documentos electrónicos, quedando esencialmente igual al original.
+        Fue popularizado en los 60s con la creación de las hojas "Letraset", las
+        cuales contenian pasajes de Lorem Ipsum, y más recientemente con
+        software de autoedición, como por ejemplo Aldus PageMaker, el cual
+        incluye versiones de Lorem Ipsum.
       </p>
-    </div>
+
+      <h2 className="my-4 text-3xl font-bold">Introduction</h2>
+      <ul className="list-inside list-disc">
+        <li>
+          <a
+            className="text-purple-800 underline decoration-solid"
+            href="https://instagram.com"
+          >
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-purple-800 underline decoration-solid"
+            href="https://github.com"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-purple-800 underline decoration-solid"
+            href="https://facebook.com"
+          >
+            Facebook
+          </a>
+        </li>
+      </ul>
+    </main>
   );
 }
