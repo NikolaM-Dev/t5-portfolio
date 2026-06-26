4a35d7d315c27a82fda5b5af21170a5fb440b099
Juan David Merchan Torres <juan1014166@gmail.com>
2026-06-26 08:52:29 -0500

    feat(contact): add base static contact page

--- src/routes/_layout/contact.tsx
+++ src/routes/_layout/contact.tsx
@@ -5,5 +5,39 @@ export const Route = createFileRoute('/_layout/contact')({
 });
 
 function RouteComponent() {
-  return <div>Hello "/contact"!</div>;
+  return (
+    <main>
+      <h1 className="my-4 text-4xl font-bold">Contact</h1>
+      <form className="flex flex-col gap-4">
+        <div className="flex items-center justify-between gap-4">
+          <label htmlFor="name">Name</label>
+          <input
+            className="rounded border px-2 py-1"
+            type="text"
+            placeholder="Name"
+            id="name"
+          />
+        </div>
+
+        <div className="flex items-center justify-between gap-4">
+          <label htmlFor="email">Email</label>
+          <input
+            className="rounded border px-2 py-1"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+
+        <div className="flex items-center justify-between gap-4">
+          <label htmlFor="message">Message</label>
+
+          <textarea
+            id="message"
+            className="rounded border px-2 py-1"
+            placeholder="Message"
+          />
+        </div>
+      </form>
+    </main>
+  );
 }
