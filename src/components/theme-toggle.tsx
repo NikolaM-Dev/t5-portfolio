bdce1f0d2176438298834c6d9c8b7edbe50d9949
Juan David Merchan Torres <juan1014166@gmail.com>
2026-06-27 11:02:22 -0500

    feat(components): add ThemeToggle component

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,13 @@
+import { useTheme } from '#/context/theme-context';
+
+export function ThemeToggle(): React.JSX.Element {
+  const { themeMode, onChange } = useTheme();
+
+  return (
+    <select onChange={onChange} value={themeMode} name="theme" id="theme">
+      <option value="auto">💻 Auto</option>
+      <option value="light">🌝 Light</option>
+      <option value="dark">🌚 Dark</option>
+    </select>
+  );
+}
