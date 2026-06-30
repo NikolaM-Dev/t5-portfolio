44ed5408c9cc681cc000aac7fc5bc4239ccf315d
Juan David Merchan Torres <juan1014166@gmail.com>
2026-06-30 11:12:27 -0500

    style(components): change to display emojis at the end

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -4,10 +4,16 @@ export function ThemeToggle(): React.JSX.Element {
   const { themeMode, onChange } = useTheme();
 
   return (
-    <select onChange={onChange} value={themeMode} name="theme" id="theme">
-      <option value="auto">💻 Auto</option>
-      <option value="light">🌝 Light</option>
-      <option value="dark">🌚 Dark</option>
+    <select
+      className="mb-4 self-start py-1"
+      onChange={onChange}
+      value={themeMode}
+      name="theme"
+      id="theme"
+    >
+      <option value="auto">Auto 💻</option>
+      <option value="light">Light 🌝</option>
+      <option value="dark">Dark 🌚</option>
     </select>
   );
 }
