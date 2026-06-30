ec832f927e85462d81e93f3db0f10f839f1475e4
Juan David Merchan Torres <juan1014166@gmail.com>
2026-06-30 15:04:18 -0500

    feat(components): change to mute a little bit the footer text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer(): React.JSX.Element {
   return (
-    <footer className="flex justify-center py-4">
+    <footer className="flex justify-center py-4 text-flexoki-600">
       ©️ {new Date().getFullYear()} NikolaM-Dev. All rights reserved.
     </footer>
   );
