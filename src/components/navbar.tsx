26422a1e738eee8976335a8982f54453185574cf
Juan David Merchan Torres <juan1014166@gmail.com>
2026-06-30 15:05:28 -0500

    refactor(components): change to use NLink, the custom link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,48 +1,13 @@
-import { Link } from '@tanstack/react-router';
+import { NLink } from '#/components/nlink';
 
 export function Navbar(): React.JSX.Element {
   return (
     <>
-      <nav className="flex flex-col justify-center  gap-4 md:flex-row">
-        <Link
-          activeProps={{
-            className: 'font-bold',
-          }}
-          className="hover:underline"
-          to="/"
-        >
-          Home
-        </Link>
-
-        <Link
-          activeProps={{
-            className: 'font-bold',
-          }}
-          className="hover:underline"
-          to="/projects"
-        >
-          Projects
-        </Link>
-
-        <Link
-          activeProps={{
-            className: 'font-bold',
-          }}
-          className="hover:underline"
-          to="/about"
-        >
-          About
-        </Link>
-
-        <Link
-          activeProps={{
-            className: 'font-bold',
-          }}
-          className="hover:underline"
-          to="/contact"
-        >
-          Contact
-        </Link>
+      <nav className="flex flex-col justify-center gap-4 md:flex-row">
+        <NLink to="/">Home</NLink>
+        <NLink to="/projects">Projects</NLink>
+        <NLink to="/about">About</NLink>
+        <NLink to="/contact">Contact</NLink>
       </nav>
     </>
   );
